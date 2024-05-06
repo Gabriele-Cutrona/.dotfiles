6921492a7367f5af57dfac7c506d0a9fad6c9899
Gabriele-Cutrona <106313541+Gabriele-Cutrona@users.noreply.github.com>
2024-05-06 14:12:53 +0200

    sudo echo now working

--- script.sh
+++ script.sh
@@ -10,12 +10,12 @@ sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-sudo echo "[chaotic-aur]" >> /etc/pacman.conf
-sudo echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+sudo sh -c "echo \"[chaotic-aur]\" >> /etc/pacman.conf"
+sudo sh -c "echo \"Include = /etc/pacman.d/chaotic-mirrorlist\" >> /etc/pacman.conf"
 ### End Chaotic AUR ###
 
 sh <(curl -L https://nixos.org/nix/install) --daemon
-sudo echo "experimental-features = nix-command flakes" >> /etc/nix/nix.conf
+sudo sh -c "echo \"experimental-features = nix-command flakes\" >> /etc/nix/nix.conf"
 
 sudo pacman -S blackbox-terminal
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
