47c34601234bdce5adc4c7fb1f0c3b2aa180727f
Gabriele-Cutrona <106313541+Gabriele-Cutrona@users.noreply.github.com>
2024-06-05 16:43:36 +0200

    neoscroll

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -167,6 +167,12 @@ require('lazy').setup({
      config = function()
         require("supermaven-nvim").setup({})
      end,
+  },
+  {
+     "karb94/neoscroll.nvim",
+     config = function()
+        require("neoscroll").setup({})
+     end,
   }
 })
 -- End Installing Plugins (with lazy)
@@ -207,7 +213,6 @@ remap("n", "<Space>lg", "<cmd>LazyGit<CR>", {noremap = true, silent = true})
 remap("n", "<Space>i", "<cmd>set tabstop=2 | set shiftwidth=2 | set softtabstop=2<CR>", {noremap = true, silent = true})
 remap("n", "<Space>o", "<cmd>set tabstop=3 | set shiftwidth=3 | set softtabstop=3<CR>", {noremap = true, silent = true})
 remap("n", "<Space>p", "<cmd>set tabstop=4 | set shiftwidth=4 | set softtabstop=4<CR>", {noremap = true, silent = true})
-
 -- End Keymaps
 
 -- Plugins Setup
