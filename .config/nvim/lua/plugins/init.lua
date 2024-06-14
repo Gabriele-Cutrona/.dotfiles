3b9964f201df1d9a388c0987eb828d6ce216e4ea
Gabriele Cutrona <106313541+Gabriele-Cutrona@users.noreply.github.com>
2024-06-14 12:46:50 +0200

    nvim-surround

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -36,6 +36,14 @@ return {
       "windwp/nvim-ts-autotag",
    },
    {
+      "kylechui/nvim-surround",
+      version = "*", -- Use for stability; omit to use `main` branch for the latest features
+      event = "VeryLazy",
+      config = function()
+         require("nvim-surround").setup({})
+      end,
+   },
+   {
       "kdheepak/lazygit.nvim",
       cmd = {
          "LazyGit",
