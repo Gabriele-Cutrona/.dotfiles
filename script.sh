567936fa912fcad02da2c6be77f2878500cf2cad
Gabriele Cutrona <106313541+Gabriele-Cutrona@users.noreply.github.com>
2024-12-26 00:42:23 +0100

    ✨ feat(tooling): add nix + install fnm via nix

--- script.sh
+++ script.sh
@@ -46,10 +46,14 @@ fi
 echo "Installing hyprland"
 sudo pacman -Sy hyprland hyprpaper hyprlock hypridle polkit-gnome xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
 
-echo "Installing papirus-icon-theme swaync fastfetch (hyfetch) eza bat sl zoxide fzf cava neovim yazi wl-clipboard lazygit pamixer brightnessctl grimblast fnm"
+echo "Installing papirus-icon-theme swaync fastfetch (hyfetch) eza bat sl zoxide fzf cava neovim yazi wl-clipboard lazygit pamixer brightnessctl grimblast"
 sudo pacman -S papirus-icon-theme swaync
 sudo pacman -Sy fastfetch extra/onefetch hyfetch eza bat less sl lolcat toilet zoxide fzf neovim yazi ripgrep fd wl-clipboard lazygit pamixer brightnessctl
-paru -S cava grimblast fnm-bin
+paru -S cava grimblast
+
+sh <(curl -L https://nixos.org/nix/install) --daemon --yes
+export PATH=$HOME/.nix-profile/bin:/nix/var/nix/profiles/default/bin:$PATH
+nix profile install nixpkgs#fnm --extra-experimental-features nix-command --extra-experimental-features flakes
 eval "`fnm env`"
 fnm use --install-if-missing 23
 npm install -g gitmoji-cli
