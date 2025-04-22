256fab82d256117d9059fce9e7425f69b5006f05
Gabriele Cutrona <106313541+Gabriele-Cutrona@users.noreply.github.com>
2025-04-22 21:57:25 +0200

    🐛 fix(nvim): rename remap to Remap since it's global

--- .config/nvim/lua/keymaps/indent.lua
+++ .config/nvim/lua/keymaps/indent.lua
@@ -1,26 +1,26 @@
 -- Froms spaces to tabs: <Leader>i<number>, <Leader>it, <Leader>ir
 -- Froms tabs to spaces: <Leader>i<number>, <Leader>is, <Leader>ir
 
-remap(
+Remap(
    "n",
    "<Leader>i2",
    "<cmd>set tabstop=2 | set shiftwidth=2 | set softtabstop=2<CR>",
    { noremap = true, silent = true }
 )
-remap(
+Remap(
    "n",
    "<Leader>i3",
    "<cmd>set tabstop=3 | set shiftwidth=3 | set softtabstop=3<CR>",
    { noremap = true, silent = true }
 )
-remap(
+Remap(
    "n",
    "<Leader>i4",
    "<cmd>set tabstop=4 | set shiftwidth=4 | set softtabstop=4<CR>",
    { noremap = true, silent = true }
 )
 
-remap("n", "<Leader>is", "<cmd>lua vim.opt.expandtab=true<CR>", { noremap = true, silent = true })
-remap("n", "<Leader>it", "<cmd>lua vim.opt.expandtab=false<CR>", { noremap = true, silent = true })
+Remap("n", "<Leader>is", "<cmd>lua vim.opt.expandtab=true<CR>", { noremap = true, silent = true })
+Remap("n", "<Leader>it", "<cmd>lua vim.opt.expandtab=false<CR>", { noremap = true, silent = true })
 
-remap("n", "<Leader>ir", "<cmd>%retab!<CR>", { noremap = true, silent = true })
+Remap("n", "<Leader>ir", "<cmd>%retab!<CR>", { noremap = true, silent = true })
