0ec2e17e778c1d0c142bf0782b74e894953cca0a
Gabriele Cutrona <106313541+Gabriele-Cutrona@users.noreply.github.com>
2025-05-10 09:50:56 +0200

    ✨ feat: scroll hypr workspaces (mod+ctrl+h/l)

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -7,14 +7,6 @@ return {
          vim.cmd("colorscheme catppuccin-mocha")
       end,
    },
-   -- {
-   --    "akinsho/bufferline.nvim",
-   --    version = "*",
-   --    dependencies = "nvim-tree/nvim-web-devicons",
-   --    config = function()
-   --       require("bufferline").setup({})
-   --    end,
-   -- },
    {
       "nvim-lualine/lualine.nvim",
       dependencies = "nvim-tree/nvim-web-devicons",
@@ -28,19 +20,12 @@ return {
          }
       end,
    },
-   -- {
-   --    "karb94/neoscroll.nvim",
-   --    config = function()
-   --       require("neoscroll").setup({})
-   --    end,
-   -- },
    {
       "folke/noice.nvim",
       event = "VeryLazy",
       opts = {},
       dependencies = {
          "MunifTanjim/nui.nvim",
-         -- "rcarriga/nvim-notify",
       },
    },
 }
