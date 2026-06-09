14cc2418c723828a45884ccfbed00fe17f44c25f
Gabriele Cutrona <106313541+Gabriele-Cutrona@users.noreply.github.com>
2026-06-09 02:43:14 +0200

    🔥 chore: remove ludsavi

--- script.sh
+++ script.sh
@@ -200,8 +200,6 @@ if [[ $APPS == "y" ]]; then
 	paru -S heroic-games-launcher-bin
 	flatpak install flathub org.vinegarhq.Sober
 
-	flatpak install flathub com.github.mtkennerly.ludusavi
-
 	flatpak install flathub io.github.wivrn.wivrn
 	paru -S wayvr-bin --noconfirm
 	
