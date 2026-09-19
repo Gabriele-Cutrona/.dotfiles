765df230bab69b142c1cfcba34a65792778353df
Gabriele Cutrona <106313541+Gabriele-Cutrona@users.noreply.github.com>
2026-09-19 14:23:36 +0200

    🔥 chore(nvim): remove redundant comment

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -45,8 +45,4 @@ return {
       opts = {}, -- for default options, refer to the configuration section for custom setup.
       cmd = "Trouble",
    },
-	-- {
-	-- 	"davidmh/mdx.nvim",
-	-- 	dependencies = {"nvim-treesitter/nvim-treesitter"}
-	-- }
 }
